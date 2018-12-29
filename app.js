a2e3be0725ad4570482fd93202193bb6e77337e2
Vishesh <visheshsingh16@gmail.com>
2018-12-29 14:34:23 -0500

    MongoDB connected

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+
+// App
 const app = express();
+
+// CONNECT TO DB
+const db = require("./config/keys").MongoURI;
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
@@ -12,4 +25,4 @@ app.use("/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Serving started on port ${PORT}...`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://vishesh:test123@mongodb-passport-auth-vp7yg.mongodb.net/test?retryWrites=true"
+};
