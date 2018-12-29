2834bbf129e13ab4aeedf26fcd0e78216caf4bac
Vishesh <visheshsingh16@gmail.com>
2018-12-29 18:27:57 -0500

    Logging in with user details

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const { ensureAuthenticated } = require("../config/auth");
 // WELCOME
 router.get("/", (req, res) => res.render("welcome"));
 // DASHBOARD
-router.get("/dashboard", ensureAuthenticated, (req, res) => res.render("dashboard"));
+router.get("/dashboard", ensureAuthenticated, (req, res) => res.render("dashboard", {
+    name: req.user.name
+}));
 
 module.exports = router;
