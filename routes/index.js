63c3a077b7f47fc3eb2313ff78b4377eba3d566c
Vishesh <visheshsingh16@gmail.com>
2018-12-29 18:13:25 -0500

    Redirecting to Dashboard after login

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// WELCOME
 router.get("/", (req, res) => res.render("welcome"));
+// DASHBOARD
+router.get("/dashboard", (req, res) => res.render("dashboard"));
 
 module.exports = router;
