4eef3177ceb4207d3a4e0b38973614cbe28847dc
Vishesh <visheshsingh16@gmail.com>
2018-12-29 18:25:37 -0500

    Protecting route by authenticating

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuthenticated } = require("../config/auth");
 
 // WELCOME
 router.get("/", (req, res) => res.render("welcome"));
 // DASHBOARD
-router.get("/dashboard", (req, res) => res.render("dashboard"));
+router.get("/dashboard", ensureAuthenticated, (req, res) => res.render("dashboard"));
 
 module.exports = router;
